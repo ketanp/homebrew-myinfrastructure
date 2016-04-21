7dcd2de3aeb96ee66728908079fedd37bf767f08
kpatel <ketpat8@gmail.com>
2016-04-21 11:41:50 -0400

    Created a brew script

---
+++ myinfrastructure.rb
@@ -0,0 +1,11 @@
+class Myinfrastructure < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/ketanp/myinfrastructure/archive/1.0.3.tar.gz"
+  version "1.0.3"
+  sha256 "5ae9b4219ed80eecacd69061feef265afba7542a11e39848c137cb982e43cff7"
+
+  def install
+	bin.install "myinfrastructure"
+  end
+end
